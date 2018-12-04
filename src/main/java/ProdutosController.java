f0a69d0114ddf6271c5f165a90e821924099d877
Diogo Casagrande <diogo.casagrande@duxus.com.br>
2018-12-04 16:44:39 -0200

    Pag. 64 - ADD redirectTo para listar apos produtos/form ser submetido ProdutosController

--- src/main/java/ProdutosController.java
+++ src/main/java/ProdutosController.java
@@ -1,16 +1,19 @@
+import java.util.List;
+
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 import dao.ProdutoDao;
 import model.Produto;
-
-import java.util.List;
 
 @Resource
 public class ProdutosController {
 
     private final ProdutoDao dao;
-
-    public ProdutosController(ProdutoDao dao){
+    private final Result result;
+    
+    public ProdutosController(ProdutoDao dao, Result result){
         this.dao = dao;
+        this.result = result;
     }
     public List<Produto> lista(){
         System.out.println("lista");
@@ -19,6 +22,7 @@ public class ProdutosController {
     
     public void adiciona(Produto produto) {
     	dao.salve(produto);
+    	result.redirectTo(this).lista();
     }
     
     public void formulario() {
